3c234fa87713bb3ac0e2a65203a3c8ddde34b809
Nats <ns>
2019-09-19 18:49:46 -0600

    cambios

--- clase6.py
+++ clase6.py
@@ -1,10 +1,10 @@
-from gtts import gtts
+from gtts import gTTS
 
 
 def tts(text_file, lang, name_file):
     with open(text_file, "r") as file:
         text = file.read()
-    file = gtts(text=text, lang=lang)
+    file = gTTS(text=text, lang=lang)
     filename = name_file
     file.save(filename)
 
